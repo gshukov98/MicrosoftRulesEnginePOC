8a526ec20d96308683af4b9df0720d7f5c306597
Georgi Shukov <georgi.shukov98@gmail.com>
2023-07-11 11:44:48 +0300

    Adds base rules engine setup

--- src/RulesEngine.Service/Program.cs
+++ src/RulesEngine.Service/Program.cs
@@ -3,6 +3,7 @@ using RulesEngine.Service;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddLogging();
         services.AddHostedService<Worker>();
     })
     .Build();
--- src/RulesEngine.Service/Worker.cs
+++ src/RulesEngine.Service/Worker.cs
@@ -17,18 +17,100 @@ namespace RulesEngine.Service
         {
             PersonalEverydayOrderRulesEngine rulesEngineTest = new PersonalEverydayOrderRulesEngine();
             RulesEngine rulesEngine = rulesEngineTest.ConstructRulesEngine();
+            await SucessfulAsync(rulesEngine);
+            await FailedUserIsNotVipAsync(rulesEngine);
+            await FailedNotOnetimeOrderAsync(rulesEngine);
+            await FailedRefundedOrderAsync(rulesEngine);
+            await FailedAllNestedRulesFailedAsync(rulesEngine);
+        }
 
-            PersonalEverydayOrderInput input = new PersonalEverydayOrderInput("123", true, true, false);
-            List<RuleResultTree> result = await rulesEngine.ExecuteAllRulesAsync("Personal Everyday Order Workflow", input);
+        private async Task SucessfulAsync(RulesEngine rulesEngine)
+        {
+            PersonalEverydayOrderInput successfulInput = new PersonalEverydayOrderInput("123", true, true, false);
+            List<RuleResultTree> resultSuccessful = await rulesEngine.ExecuteAllRulesAsync("Personal Everyday Order Workflow", successfulInput);
 
-            result.OnSuccess((eventName) =>
+            _logger.LogInformation("Executing with successful input.");
+
+            resultSuccessful.OnSuccess((eventName) =>
             {
-                _logger.LogInformation("Achieved discount %eventName", eventName);
+                _logger.LogInformation("Multiplier {eventName}", eventName);
             });
 
-            result.OnFail(() =>
+            resultSuccessful.OnFail(() =>
             {
                 _logger.LogInformation("Failed to achieve discount");
+            });
+        }
+
+        private async Task FailedUserIsNotVipAsync(RulesEngine rulesEngine)
+        {
+            PersonalEverydayOrderInput successfulInput = new PersonalEverydayOrderInput("123", false, true, false);
+            List<RuleResultTree> resultSuccessful = await rulesEngine.ExecuteAllRulesAsync("Personal Everyday Order Workflow", successfulInput);
+
+            _logger.LogInformation("Executing with not vip user.");
+
+            resultSuccessful.OnSuccess((eventName) =>
+            {
+                _logger.LogInformation("Multiplier {eventName}", eventName);
+            });
+
+            resultSuccessful.OnFail(() =>
+            {
+                _logger.LogInformation("Failed to achieve discount.");
+            });
+        }
+
+        private async Task FailedNotOnetimeOrderAsync(RulesEngine rulesEngine)
+        {
+            PersonalEverydayOrderInput successfulInput = new PersonalEverydayOrderInput("123", true, false, false);
+            List<RuleResultTree> resultSuccessful = await rulesEngine.ExecuteAllRulesAsync("Personal Everyday Order Workflow", successfulInput);
+
+            _logger.LogInformation("Executing with not onetime order.");
+
+            resultSuccessful.OnSuccess((eventName) =>
+            {
+                _logger.LogInformation("Multiplier {eventName}", eventName);
+            });
+
+            resultSuccessful.OnFail(() =>
+            {
+                _logger.LogInformation("Failed to achieve discount.");
+            });
+        }
+
+        private async Task FailedRefundedOrderAsync(RulesEngine rulesEngine)
+        {
+            PersonalEverydayOrderInput successfulInput = new PersonalEverydayOrderInput("123", true, true, true);
+            List<RuleResultTree> resultSuccessful = await rulesEngine.ExecuteAllRulesAsync("Personal Everyday Order Workflow", successfulInput);
+
+            _logger.LogInformation("Executing with refunded order.");
+
+            resultSuccessful.OnSuccess((eventName) =>
+            {
+                _logger.LogInformation("Multiplier {eventName}", eventName);
+            });
+
+            resultSuccessful.OnFail(() =>
+            {
+                _logger.LogInformation("Failed to achieve discount.");
+            });
+        }
+
+        private async Task FailedAllNestedRulesFailedAsync(RulesEngine rulesEngine)
+        {
+            PersonalEverydayOrderInput successfulInput = new PersonalEverydayOrderInput("123", false, false, true);
+            List<RuleResultTree> resultSuccessful = await rulesEngine.ExecuteAllRulesAsync("Personal Everyday Order Workflow", successfulInput);
+
+            _logger.LogInformation("Executing with all rules failed.");
+
+            resultSuccessful.OnSuccess((eventName) =>
+            {
+                _logger.LogInformation("Multiplier {eventName}", eventName);
+            });
+
+            resultSuccessful.OnFail(() =>
+            {
+                _logger.LogInformation("Failed to achieve discount.");
             });
         }
     }
